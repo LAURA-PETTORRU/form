81b4ad6f7b786af76081ea4f5d809810b50a73d0
LAURA-PETTORRU <laura.pettorru@yahoo.it>
2017-05-28 17:08:28 +0200

    Add files via upload

---
+++ form/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="jumbotron">
+		<div class="container">
+			
+		<!-- navbar -->
+		<?php include("fragments/nav.html"); ?>
+		<!-- welcome -->
+		<?php include("fragments/welcome.html"); ?>
+
+	</div>
+</div>
+
+
+		<!-- form -->
+		<?php include("fragments/form.html"); ?>
+
+		<script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+		<script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+	</html>
